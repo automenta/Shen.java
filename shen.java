82ef4002482c5ebd620abd7207b9cebb63659116
abc <artella.coding@googlemail.com>
2013-12-02 09:27:11 +0000

    updating shen.java script

--- shen.java
+++ shen.java
@@ -2,7 +2,14 @@
 
 rlwrap=$(which rlwrap) || "" &> /dev/null
 java="$rlwrap $JAVA_HOME/bin/java $JAVA_OPTS"
-shen="find . -name shen.java-*.jar"
 
-test -z `$shen` && mvn package
+if [ "`which shen.java`" != "" ]; then
+  DIR=$(dirname `which shen.java`)
+else
+  DIR="."
+fi
+
+shen="find $DIR -name shen.java-*.jar"
+
+test -z `$shen` && mvn -f $DIR"/pom.xml" package
 $java -Xss1000k -jar `$shen`
